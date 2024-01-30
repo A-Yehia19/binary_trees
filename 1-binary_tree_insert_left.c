78d9cf1dde13d2d19cf64c8bd9ba538681c273fb
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-01-30 21:52:45 +0200

    [update]: redifine the functions inside the module

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,6 +1,28 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_node_new - creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ *
+ * Return: pointer to the new node, or NULL on failure
+*/
+binary_tree_t *binary_tree_node_new(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	node->parent = parent;
+	node->n = value;
+	node->right = NULL;
+	node->left = NULL;
+
+	return (node);
+}
+
+/**
  * binary_tree_insert_left - insert a binary tree node in left branch
  * @parent: pointer to the parent node of the node to create
  * @value: value to put in the new node
@@ -10,7 +32,7 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *left_node = parent->left;
-	binary_tree_t *node = binary_tree_node(parent, value);
+	binary_tree_t *node = binary_tree_node_new(parent, value);
 
 	if (node == NULL)
 		return (NULL);
